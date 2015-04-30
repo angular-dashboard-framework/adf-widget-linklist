d231fd2bbbec4b1b30d929da05209173f3539182
Sebastian Sdorra <s.sdorra@gmail.com>
2015-04-30 20:26:57 +0200

    release v0.1.1

--- dist/adf-widget-linklist.js
+++ dist/adf-widget-linklist.js
@@ -1,3 +1,4 @@
+(function(window, undefined) {'use strict';
 /* *
  * The MIT License
  *
@@ -22,10 +23,10 @@
  * THE SOFTWARE.
  */
 
-'use strict';
+
 
 angular.module('adf.widget.linklist', ['adf.provider'])
-  .config(function(dashboardProvider){
+  .config(["dashboardProvider", function(dashboardProvider){
     dashboardProvider
       .widget('linklist', {
         title: 'Links',
@@ -39,12 +40,12 @@ angular.module('adf.widget.linklist', ['adf.provider'])
           controller: 'linklistEditCtrl'
         }
       });
-  }).controller('linklistCtrl', function($scope, config){
+  }]).controller('linklistCtrl', ["$scope", "config", function($scope, config){
     if (!config.links){
       config.links = [];
     }
     this.links = config.links;
-  }).controller('linklistEditCtrl', function($scope){
+  }]).controller('linklistEditCtrl', ["$scope", function($scope){
     function getLinks(){
       if (!$scope.config.links){
         $scope.config.links = [];
@@ -57,7 +58,7 @@ angular.module('adf.widget.linklist', ['adf.provider'])
     $scope.removeLink = function(index){
       getLinks().splice(index, 1);
     };
-  });
+  }]);
 
 angular.module("adf.widget.linklist").run(["$templateCache", function($templateCache) {$templateCache.put("{widgetsPath}/linklist/src/edit.html","<form class=form-inline role=form><div><label>Links</label></div><div class=padding-bottom ng-repeat=\"link in config.links\"><div class=form-group><label class=sr-only for=title-{{$index}}>Title</label> <input type=text id=title-{{$index}} class=form-control placeholder=Title ng-model=link.title required></div><div class=form-group><label class=sr-only for=href-{{$index}}>URL</label> <input type=url id=href-{{$index}} class=form-control placeholder=http://example.com ng-model=link.href required></div><button type=button class=\"btn btn-warning\" ng-click=removeLink($index)><i class=\"fa fa-minus\"></i> Remove</button></div><button type=button class=\"btn btn-primary\" ng-click=addLink()><i class=\"fa fa-plus\"></i> Add</button></form>");
-$templateCache.put("{widgetsPath}/linklist/src/view.html","<div class=linklist><ul><li ng-repeat=\"link in list.links | orderBy:\'title\'\"><a target=_blank ng-href={{link.href}}>{{link.title}}</a></li></ul></div>");}]);
+$templateCache.put("{widgetsPath}/linklist/src/view.html","<div class=linklist><ul><li ng-repeat=\"link in list.links | orderBy:\'title\'\"><a target=_blank ng-href={{link.href}}>{{link.title}}</a></li></ul></div>");}]);})(window);
